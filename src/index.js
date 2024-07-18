ff10f75f4446075e8c3269497b37dbbf8545e1e5
GavinCXY <98086322+GavinCXY@users.noreply.github.com>
2024-07-18 15:48:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   //"ecr.libcuda.so": "https://public.ecr.aws",
 
   // staging
-  //"docker-staging.libcuda.so": dockerHub,
+  "chan-qian.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
